f8ba43254f98cd0682caf255824dc1c1eaf41d52
zahra-dev8 <zehra.polatdemir.dev@gmail.com>
2026-09-25 21:01:07 +0300

    Update main.py

--- Day_14_Higher_or_Lower/main.py
+++ Day_14_Higher_or_Lower/main.py
@@ -7,39 +7,61 @@ def get_random_account():
     a_kisi, b_kisi = random.sample(game_data.data, 2)
     return a_kisi, b_kisi
 
-account_a, account_b = get_random_account()
+def format_data(account):
+    formatted_string = f"{account['name']}, a {account['description']}, from {account['country']}"
+    return formatted_string
 
-def check_answer():
+
+def check_answer(current_score, a_account, b_account):
     print(art.logo)
-    if score > 0:
-        print(f"You're right! Current score: {score}.")
-    print(f"Compare A: {account_a['name']}, a {account_a['description']}, from {account_a['country']}.")
+    if current_score > 0:
+        print(f"You're right! Current score: {current_score}.")
+
+    print(f"Compare A: {format_data(a_account)}")
     print(art.vs)
-    print(f"Against B: {account_b['name']}, a {account_b['description']}, from {account_b['country']}.")
+    print(f"Against B: {format_data(b_account)}")
+
     return input("Who has more followers? Type 'A' or 'B': ").lower()
 
-
-winner = ""
-score = 0
-game_should_continue = True
-while game_should_continue:
-    choice = check_answer()
-    if account_a['follower_count'] > account_b['follower_count']:
-        winner = account_a
+def check_guess(account_a, account_b, guess):
+    if account_a > account_b:
+        return guess == "a"
     else:
-        winner = account_b
+        return guess == "b"
+
+def play_game():
+
+    score = 0
+    game_should_continue = True
+    account_a, account_b = get_random_account()
 
 
-    if (choice == "a" and winner == account_a) or (choice == "b" and winner == account_b):
-        score += 1
+    while game_should_continue:
+        choice = check_answer(score, account_a, account_b)
+
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+
+        is_correct = check_guess(a_follower_count,b_follower_count,choice)
         os.system("clear")
-        account_a = account_b
-        account_b = random.choice(game_data.data)
-        while account_a == account_b:
+
+        if is_correct:
+            score += 1
+            account_a = account_b
             account_b = random.choice(game_data.data)
+            while account_a == account_b:
+                account_b = random.choice(game_data.data)
 
-    else:
-        os.system("clear")
-        print(art.logo)
-        print(f"Sorry, that's wrong. Final score: {score}")
-        game_should_continue = False
+        else:
+            print(art.logo)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            game_should_continue = False
+
+play_game()
+
+
+
+
+
+
+
